0432224bd97b4e0f6eeb9d9eb530995a3daaa550
zhaolion <zhaoliangsyn@gmail.com>
2026-04-10 16:12:57 +0800

    feat: add @opentab/api package with tRPC router and health endpoint

---
+++ packages/api/src/index.ts
@@ -0,0 +1,3 @@
+export { type Context, createContextFactory } from "./context.js";
+export { type AppRouter, appRouter } from "./routers/index.js";
+export { protectedProcedure, publicProcedure, router } from "./trpc.js";
