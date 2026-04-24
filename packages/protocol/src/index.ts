ec6a0dfcc9890998a79eb9096f965ad930c07f28
zhaolion <zhaoliangsyn@gmail.com>
2026-04-24 18:46:01 +0800

    feat(protocol): barrel exports

--- packages/protocol/src/index.ts
+++ packages/protocol/src/index.ts
@@ -1,4 +1,14 @@
-// Protocol package for shared Zod schemas
-// Real schemas will be added in subsequent tasks
+// Barrel exports for the @opentab/protocol package.
+// Consumers (apps/cloud server, apps/extension client) import zod schemas
+// and constants from this single entrypoint.
 
-export const PROTOCOL_VERSION = "0.0.0";
+export * from "./constants";
+export * from "./endpoints/exchange-consume";
+export * from "./endpoints/health";
+export * from "./endpoints/pull";
+export * from "./endpoints/push";
+export * from "./endpoints/snapshot";
+export * from "./entities";
+export * from "./errors";
+export * from "./ops";
+export * from "./version";
